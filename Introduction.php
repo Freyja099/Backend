1eaf8704031f7af64d719b4e3581f1a0e5d745f0
Aya Idrissi <aya.zoujari.id@gmail.com>
2024-12-03 13:37:40 +0100

    Intro

---
+++ Introduction.php
@@ -0,0 +1,31 @@
+<?php
+// * Session 1 : Introduction 
+
+// * php is a general-purpose scripting language that is especially suited to web development.
+// * It's used to build a dynamic web pages, it runs on a server not the user's web browser
+// * It was released in 1995 and php was originally an acronym "for personal home page" but that was later changed to "php hypertext preprocessor"
+
+// * How does PHP work?
+// * The browser send a request to a server, PHP on that server processes that request then the server sends HTML back to the browser, the server running PHP can even communicate to a database using sql query 
+
+// * To write php code we use <?php  code   ? >
+// * To display a message we use echo either double quote or single quote and a semi colone at the end, that way : echo "message " ;
+// * To add a line break we can use <br>
+
+// * PHP file can contain HTML, CSS, JavaScript and php
+
+echo "Hello World! <br>";
+echo " this is another line";
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+
+    <body>
+        <button> button </button>
+    </body>
+</html>
